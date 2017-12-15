5ffa079db98d594df27ec09aabaf07a7ee60b13d
Gonzalo Benitez <info@arvosesh.com>
2017-12-16 10:09:01 +1100

    fix error page

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,37 +1,41 @@
 <template>
-  <section class="container">
-    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
-      {{ error.statusCode }}
-    </h1>
-    <h2 class="info">
-      {{ error.message }}
-    </h2>
-    <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
-      Homepage
-    </nuxt-link>
-  </section>
+   <v-app>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-card>
+            <v-card-media :src="backgroundUrl" height="300px">
+            </v-card-media>
+            <v-card-title primary-title>  
+              <v-flex xs12 align-end flexbox>    
+                <h1 class="text-xs-center">
+                  {{ error.statusCode }}
+                </h1>
+                <h2>
+                  {{ error.message }}
+                </h2>
+              </v-flex>
+            </v-card-title>
+            <v-card-actions>
+              <nuxt-link to="/" v-if="error.statusCode === 404">
+                Go Homepage
+              </nuxt-link>
+            </v-card-actions>
+          </v-card>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
+
 <script>
+import backgroundUrl from '~/assets/img/logo.png'
 export default {
-  props: ['error']
+  props: ['error'],
+  data() {
+    return {
+      backgroundUrl: backgroundUrl
+    }
+  }
 }
 </script>
-
-<style scoped>
-.title
-{
-  margin-top: 15px;
-  font-size: 5em;
-}
-.info
-{
-  font-weight: 300;
-  color: #9aabb1;
-  margin: 0;
-}
-.button
-{
-  margin-top: 50px;
-}
-</style>
